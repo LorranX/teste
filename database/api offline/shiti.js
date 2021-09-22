dda97247969084fa60504d05a5eb0e4ec4cd647d
LorranX <89737304+LorranX@users.noreply.github.com>
2021-09-21 22:42:19 -0300

    Delete database/api offline directory

--- database/api offline/shiti.js
+++
@@ -1,4 +0,0 @@
-[
-    {"result": "https://ibb.co/tMf9fPH?clickid=6557dc43-272c-4944-bced-1f3f7041049b&pub=5b9248587c4b9603af2ba106&sub_pub_id=API"},
-    {"result": "https://i.ibb.co/GQnNzg1/Whats-App-Image-2021-09-19-at-12-02-06-PM.jpg"}
-    ]
--- database/api offline/shitv.js
+++
@@ -1,3 +0,0 @@
-[
-    {"result": "https://drive.google.com/drive/u/1/folders/1gI-lM106z65IuPH89DVRMrx-aYw2j8vA"}
-    ]
