6d268b82a66184bae552de353f7fac05f8896eec
LorranX <89737304+LorranX@users.noreply.github.com>
2021-10-10 10:00:17 -0300

    Add files via upload

--- respostas/RESPOSTAS.js
+++ respostas/RESPOSTAS.js
@@ -8,6 +8,10 @@ exports.wait2 = () => {
 	return`*Calmai macaco, pode ser que demore um pouco 🦧*`
 }
 
+exports.banido = () => {
+	return`*Eita zap, por algum motivo você esta proibido de usar meus comandos, converse com meu dono*`
+}
+
 exports.uangkau = (pushname, sender, uangkau) => {
 	return`*┏⊱ 「 𝘾𝘼𝙍𝙏𝙀𝙄𝙍𝘼 𝘿𝙀 𝙋𝙊𝙉𝙏𝙊𝙎 💵 」⊰━┓*\n┣⊱ *Nome* : ${pushname}\n┣⊱ *Número* : ${sender.split("@")[0]}\n┣⊱ *Pontos* : ${uangkau}\n┗━━━━━━━━━━`
 }
